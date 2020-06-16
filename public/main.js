4285453f93094caf3711c0e4f30d228fd326b5a5
binny1996 <binson.jimmy@gmail.com>
2020-06-16 12:05:32 +0530

    second commit

--- public/main.js
+++ public/main.js
@@ -3,7 +3,7 @@ var comments = [{"username":"James","comments":"This looks sad"},{"username":"Ja
 
 if(localStorage.getItem("bookmarked")){
   var bookmarks = []
-  bookmarks.push(JSON.parse(localStorage.getItem("bookmarked")))
+  bookmarks = bookmarks.concat(JSON.parse(localStorage.getItem("bookmarked")))
 }else{
   var bookmarks = []
 }
@@ -19,51 +19,67 @@ response.onload = function () {
 
   if (response.status >= 200 && response.status < 400) {
     data.map((feed, index) => {
-      hook.insertAdjacentHTML('beforeend', '<div class="single-post">'+
-      '<div class="user-wrap">'+
-        '<div class="user-left">'+
-          '<div class="user-img-wrap">'+
-            '<img class="user-img" src="'+feed['low thumbnail']+'" />'+
+            hook.insertAdjacentHTML('beforeend', '<div class="single-post">'+
+        '<div class="user-wrap">'+
+          '<div class="user-left">'+
+            '<div class="user-img-wrap">'+
+              '<img class="user-img" src="'+feed['low thumbnail']+'" />'+
+            '</div>'+
+            '<span class="username">varun.aditya</span>'+
           '</div>'+
-          '<span class="username">varun.aditya</span>'+
+          '<div class="user-right">'+
+            '<img src="images/more.png" class="settings" />'+
+          '</div>'+
         '</div>'+
-        '<div class="user-right">'+
-          '<img src="images/more.png" class="settings" />'+
+        '<img class="user-feed-img" src="'+feed['high thumbnail']+'" />'+
+        '<div class="user-action-wrap">'+
+          '<div class="action-left">'+
+            '<img src="images/heart.png" class="action-icon-left" />'+
+            '<img src="images/comment.png" class="action-icon-left" />'+
+            '<img src="images/send.png" class="action-icon-left" />'+
+          '</div>'+
+          '<div class="action-right">'+
+            '<img id="bookmark'+index+'" src="images/bookmark.png" class="action-icon-right bookmark" />'+
+          '</div>'+
         '</div>'+
-      '</div>'+
-      '<img class="user-feed-img" src="'+feed['high thumbnail']+'" />'+
-      '<div class="user-action-wrap">'+
-        '<div class="action-left">'+
-          '<img src="images/heart.png" class="action-icon-left" />'+
-          '<img src="images/comment.png" class="action-icon-left" />'+
-          '<img src="images/send.png" class="action-icon-left" />'+
+        '<p class="feed-title"><b>varun.aditya </b>'+feed.title+'</p>'+
+        '<p class="feed-description"></p>'+
+        '<div id="comments">'+
+        comments[0].username+': '+comments[0].comments+
         '</div>'+
-        '<div class="action-right">'+
-          '<img id="'+index+'" src="images/bookmark.png" class="action-icon-right bookmark" />'+
-        '</div>'+
-      '</div>'+
-      '<p class="feed-title"><b>varun.aditya </b>'+feed.title+'</p>'+
-      '<p class="feed-description"></p>'+
-      '<div id="comments">'+
-      comments[0].username+': '+comments[0].comments+
-      '</div>'+
-    '</div>')
+      '</div>')
 
-    var bookmark = document.getElementsByClassName("bookmark")
-    bookmark[index].addEventListener("click", function(e){
-      bookmarks.push(index)
-      localStorage.setItem("bookmarked", JSON.stringify(bookmarks));
-      console.log(localStorage.getItem("bookmarked"))
+      var bookmark = document.getElementById("bookmark"+index)
+      bookmark.addEventListener("click", handleBookmark)
+
+      function handleBookmark(){
+        bookmarks.push(index)
+        localStorage.setItem("bookmarked", JSON.stringify(bookmarks));
+        document.getElementById("bookmark" + index).src="images/bookmarked.png"
+        console.log(localStorage.getItem("bookmarked"))
+      }
+      
     })
 
-  console.log(index)
-    })
+    for(var i=0; i<bookmarks.length; i++){
+      document.getElementById("bookmark"+bookmarks[i]).src= "images/bookmarked.png"
+      document.getElementById("bookmark"+bookmarks[i]).addEventListener("click", ()=>{
+        document.getElementById("bookmark"+bookmarks[i]).src="images/bookmark.png"
+        bookmarks.splice(bookmarks[i], 1)
+        localStorage.setItem("bookmarked", JSON.stringify(bookmarks))
+        console.log(localStorage.getItem("bookmarked"))
+
+      })
+    }
+
   } else {
     console.log("error")
   }
 
   
 }
+
+
 
 
 
